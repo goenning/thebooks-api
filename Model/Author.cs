1d75866a1a70028f6f13f55767af44292df55f72
goenning <me@goenning.net>
2016-11-08 21:46:21 -0200

    add book & author models

---
+++ Model/Author.cs
@@ -0,0 +1,8 @@
+namespace TheBooks.Api.Model
+{ 
+  public class Author
+  {
+      public int Id { get; private set; }
+      public string Name { get; private set; }
+  }
+}
---
+++ Model/Book.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace TheBooks.Api.Model
+{ 
+  public class Book
+  {
+      public int Id { get; private set; }
+      public string ISBN { get; private set; }
+      public string Title { get; private set; }
+      public int Pages { get; private set; }
+      public IEnumerable<Author> Authors { get; private set; }
+  }
+}
