d227dc95868f24bd6cca8b1bffed0e8b41fe4437
goenning <me@goenning.net>
2016-11-08 22:51:28 -0200

    read config from args

--- Program.cs
+++ Program.cs
@@ -1,15 +1,19 @@
 using System.IO;
 using Microsoft.AspNetCore.Hosting;
- 
+using Microsoft.Extensions.Configuration;
+
 namespace TheBooks.Api
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+
             var host = new WebHostBuilder()
             .UseKestrel()
             .UseContentRoot(Directory.GetCurrentDirectory())
+            .UseConfiguration(config)
             .UseStartup<Startup>()
             .Build();
  
