f141fac0e68951a7ba64ac005b69535930def3b1
goenning <me@goenning.net>
2016-11-10 18:49:33 -0200

    allow watch mode

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
-dotnet run --project src/TheBooks.Api/project.json
+#!/bin/bash
+
+if [ "$1" == "-w" ] ; then
+	(cd src/TheBooks.Api/; dotnet watch run)
+else
+  dotnet run --project src/TheBooks.Api/project.json
+fi 
